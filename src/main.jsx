a751e3954d8b513f0daa2f1f860d9da2417cc942
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-27 10:01:51 -0300

    feat: adding Category Context

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import ThemeProvider from "./utils/ThemeContext";
 import { UserProvider } from "./providers/UserContext";
 import App from "./App";
+import { CategoryProvider } from "./providers/CategoryContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <ThemeProvider>
         <UserProvider>
-          <App />
+          <CategoryProvider>
+            <App />
+          </CategoryProvider>
         </UserProvider>
       </ThemeProvider>
     </Router>
