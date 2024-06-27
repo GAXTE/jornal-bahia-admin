a2625a9e6844052d32fc6ba4f09ab02c1a910bbb
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-27 10:08:37 -0300

    feat: adding allPosts in table

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -4,38 +4,19 @@ import { NewsList } from "../../components/Tables/NewsList";
 import { DefaultModal } from "../../components/Modals/DefaultModal";
 import { DefaultTemplate } from "../DefaultTemplate/DefaultTemplate";
 import { TextRich } from "../../components/TextRich/TextRich";
-
-// Lista de posts de exemplo
-
-const posts = [
-  {
-    title:
-      "kojskljsgsfjgfjf jgjgfs sfgj jgsfjsfg fjg ghsjksdghsjsddskjgsdksdjsdg  djsdkjsdksdgsdkjdgksdg lbsa,bafbfssbflknalfnlknslknfs olkashlkasfhklasf lkhsaklfashklasfhafls ",
-    category: "Categoria A",
-    date: "01/01/2023",
-  },
-  { title: "Post 2", category: "Categoria B", date: "02/02/2023" },
-  { title: "Post 3", category: "Categoria C", date: "03/03/2023" },
-];
-
+import { usePostContext } from "../../providers/PostContext";
 export const News = () => {
   const [isModalOpenCreate, setIsModalOpenCreate] = useState(false);
   const [editContent, setEditContent] = useState("");
-
+  const { AllPosts } = usePostContext();
   const handleChange = (content) => {
     setEditContent(content);
   };
 
   return (
     <>
-      <DefaultTemplate
-        textButton={"Noticia"}
-        setIsModalOpenCreate={setIsModalOpenCreate}
-      >
-        <DefaultModal
-          isModalOpen={isModalOpenCreate}
-          setIsModalOpen={setIsModalOpenCreate}
-        >
+      <DefaultTemplate textButton={"Noticia"} setIsModalOpenCreate={setIsModalOpenCreate}>
+        <DefaultModal isModalOpen={isModalOpenCreate} setIsModalOpen={setIsModalOpenCreate}>
           <form action="">
             <input type="text" placeholder="Título" />
             <input type="file" placeholder="Imagem" />
@@ -44,7 +25,7 @@ export const News = () => {
           </form>
         </DefaultModal>
 
-        <NewsList array={posts} />
+        <NewsList array={AllPosts} />
       </DefaultTemplate>
     </>
   );
