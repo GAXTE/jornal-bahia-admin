5d0539b58c7ddb9f6e24c03904a740d6563ade42
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-27 10:09:12 -0300

    feat: adding post provider in main

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,18 @@ import ThemeProvider from "./utils/ThemeContext";
 import { UserProvider } from "./providers/UserContext";
 import App from "./App";
 import { CategoryProvider } from "./providers/CategoryContext";
+import { PostProvider } from "./providers/PostContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
       <ThemeProvider>
         <UserProvider>
-          <CategoryProvider>
-            <App />
-          </CategoryProvider>
+          <PostProvider>
+            <CategoryProvider>
+              <App />
+            </CategoryProvider>
+          </PostProvider>
         </UserProvider>
       </ThemeProvider>
     </Router>
