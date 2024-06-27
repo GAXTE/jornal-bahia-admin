34fb50762deb9f1fc112490c8804a9a9c600e734
DanielVrs <danielvrsdev@gmail.com>
2024-06-27 09:25:37 -0300

    feat: create component TextRich

---
+++ src/components/TextRich/TextRich.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
+export const TextRich = ({ value = "", onChange }) => {
+  const [text, setText] = useState(value);
+
+  const handleChange = (content) => {
+    setText(content);
+    if (onChange) {
+      onChange(content);
+    }
+  };
+
+  const modules = {
+    toolbar: [
+      [{ header: "1" }, { header: "2" }, { font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+  };
+
+  return (
+    <div>
+      <ReactQuill value={text} onChange={handleChange} modules={modules} />
+    </div>
+  );
+};
