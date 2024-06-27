d67a9b6c0610e6e71bdfcc074959944a43361bab
DanielVrs <danielvrsdev@gmail.com>
2024-06-27 09:26:24 -0300

    feat: add props

--- src/components/Buttons/EditButton.jsx
+++ src/components/Buttons/EditButton.jsx
@@ -1,13 +1,15 @@
 import editIcon from "../../assets/pencil.svg";
 
-export const Edit = () => {
+export const Edit = ({ isModalEdit, setIsModalEdit }) => {
   return (
     <>
-      <img
-        className="opacity-40 h-[15px] cursor-pointer hover:opacity-100 transition-opacity"
-        src={editIcon}
-        alt=""
-      />
+      <button onClick={() => setIsModalEdit(true)}>
+        <img
+          className="opacity-40 h-[15px] cursor-pointer hover:opacity-100 transition-opacity"
+          src={editIcon}
+          alt=""
+        />
+      </button>
     </>
   );
 };
