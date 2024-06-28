014d0dd0ebe775745b85409dfe46e64baa480159
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-28 09:43:22 -0300

    feat: adding add list

--- src/pages/AddsPage/AddsPage.jsx
+++ src/pages/AddsPage/AddsPage.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import { DefaultTemplate } from "../DefaultTemplate/DefaultTemplate";
 import { AddsList } from "../../components/Tables/AddsList";
+import { usePublicityContext } from "../../providers/PublicityContext";
 
 export const AddsPage = () => {
   const [isModalOpenCreate, setIsModalOpenCreate] = useState(false);
+  const { ListAllPublicity } = usePublicityContext();
   return (
     <>
-      <DefaultTemplate
-        textButton={"Propaganda"}
-        setIsModalOpenCreate={setIsModalOpenCreate}
-      >
-        <AddsList array={[]} />
+      <DefaultTemplate textButton={"Propaganda"} setIsModalOpenCreate={setIsModalOpenCreate}>
+        <AddsList array={ListAllPublicity} />
       </DefaultTemplate>
     </>
   );
