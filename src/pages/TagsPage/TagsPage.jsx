c3967d928ddcbc4d86252d1cf06c8bed6a019ec5
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-28 08:52:17 -0300

    feat: adding list tags

--- src/pages/TagsPage/TagsPage.jsx
+++ src/pages/TagsPage/TagsPage.jsx
@@ -1,16 +1,15 @@
 import { useState } from "react";
 import { TagsList } from "../../components/Tables/TagsList";
 import { DefaultTemplate } from "../DefaultTemplate/DefaultTemplate";
+import { useTagsContext } from "../../providers/TagsContext";
 
 export const TagsPage = () => {
   const [isModalOpenCreate, setIsModalOpenCreate] = useState(false);
+  const { ListAlltags } = useTagsContext();
   return (
     <>
-      <DefaultTemplate
-        textButton={"Tag"}
-        setIsModalOpenCreate={setIsModalOpenCreate}
-      >
-        <TagsList array={[]} />
+      <DefaultTemplate textButton={"Tag"} setIsModalOpenCreate={setIsModalOpenCreate}>
+        <TagsList array={ListAlltags} />
       </DefaultTemplate>
     </>
   );
