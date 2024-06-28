94fc3ebe23be0e542438edcb03466bc157c751dc
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-28 11:58:19 -0300

    feat: adding function create category

--- src/pages/CategoriesPage/CategoriesPage.jsx
+++ src/pages/CategoriesPage/CategoriesPage.jsx
@@ -4,26 +4,51 @@ import { CategoryList } from "../../components/Tables/CategoryList";
 import { DefaultTemplate } from "../DefaultTemplate/DefaultTemplate";
 import { useCategoryContext } from "../../providers/CategoryContext";
 import { DefaultInput } from "../../components/Inputs/DefaultInput";
+
 export const CategoriesPage = () => {
   const [isModalOpenCreate, setIsModalOpenCreate] = useState(false);
-  const { ListAllCategories } = useCategoryContext();
+  const { ListAllCategories, createCategory } = useCategoryContext();
+  const [categoryName, setCategoryName] = useState("");
+  const [categoryDescription, setCategoryDescription] = useState("");
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "categoryName") {
+      setCategoryName(value);
+    } else if (name === "categoryDescription") {
+      setCategoryDescription(value);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsModalOpenCreate(false);
+    const category = {
+      name: categoryName,
+      description: categoryDescription,
+    };
+    // createCategory(category);
+    console.log(category);
+  };
 
   return (
     <>
       <DefaultTemplate textButton={"Categoria"} setIsModalOpenCreate={setIsModalOpenCreate}>
         <DefaultModal isModalOpen={isModalOpenCreate} setIsModalOpen={setIsModalOpenCreate}>
-          <form action="">
+          <form onSubmit={handleSubmit}>
             <DefaultInput
-              type={"text"}
-              placeholder={"Nome da categoria"}
-              // handleInputChange={handleInputChange}
-              name={"name"}
+              type="text"
+              placeholder="Nome da categoria"
+              handleInputChange={handleInputChange}
+              name="categoryName"
+              value={categoryName}
             />
             <DefaultInput
-              type={"text"}
-              placeholder={"Descrição"}
-              // handleInputChange={handleInputChange}
-              name={"name"}
+              type="text"
+              placeholder="Descrição"
+              handleInputChange={handleInputChange}
+              name="categoryDescription"
+              value={categoryDescription}
             />
           </form>
         </DefaultModal>
