3b08171eaf6174cc8c0c1c6346aa3b7dc19687cd
Josenilsonfariasx <fariaslwork@gmail.com>
2024-06-29 10:12:59 -0300

    feat: adding functions create and delete in tagContext

--- src/providers/TagsContext.jsx
+++ src/providers/TagsContext.jsx
@@ -14,11 +14,31 @@ export const TagsProvider = ({ children }) => {
       setListAllTags(data);
     } catch (error) {}
   };
+  const createTag = async (name) => {
+    const obj = {
+      name: name,
+    };
+    try {
+      const { data } = await Api.post("/tag", obj);
+      console.log(data);
+      getAllTags();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const deleteTag = async (id) => {
+    try {
+      const { data } = await Api.delete(`/tag/${id}`);
+      getAllTags();
+    } catch (error) {}
+  };
   useEffect(() => {
     getAllTags();
   }, []);
 
-  return <TagsContext.Provider value={{ ListAlltags }}>{children}</TagsContext.Provider>;
+  return (
+    <TagsContext.Provider value={{ ListAlltags, createTag, deleteTag }}>{children}</TagsContext.Provider>
+  );
 };
 
 export const useTagsContext = () => useContext(TagsContext);
