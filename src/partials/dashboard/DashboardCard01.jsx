972a9fcecc66e353d2deee2543df661e136ef9d0
Josenilsonfariasx <fariaslwork@gmail.com>
2024-07-02 11:03:36 -0300

    feat: adding number of publicitys

--- src/partials/dashboard/DashboardCard01.jsx
+++ src/partials/dashboard/DashboardCard01.jsx
@@ -18,10 +18,12 @@ function DashboardCard01() {
           {/* Menu button */}
           <EditMenu align="right" className="relative inline-flex"></EditMenu>
         </header>
-        <h2 className="text-lg font-semibold text-slate-800 dark:text-slate-100 mb-2">Numero de Posts</h2>
+        <h2 className="text-lg font-semibold text-slate-800 dark:text-slate-100 mb-2">Numero de Notícias</h2>
         <div className="text-xs font-semibold text-slate-400 dark:text-slate-500 uppercase mb-1"></div>
         <div className="flex items-start">
-          <div className="text-3xl font-bold text-slate-800 dark:text-slate-100 mr-2">{posts.length}</div>
+          <div className="text-3xl font-bold text-slate-800 dark:text-slate-100 mr-2 mb-8">
+            {posts.length}
+          </div>
           {/* <div className="text-sm font-semibold text-white px-1.5 bg-emerald-500 rounded-full">+49%</div> */}
         </div>
       </div>
