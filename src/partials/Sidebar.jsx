c5247bf663b48d117eee337771d9c7033cb535a0
Josenilsonfariasx <fariaslwork@gmail.com>
2024-07-02 16:38:49 -0300

    fix: sidebar

--- src/partials/Sidebar.jsx
+++ src/partials/Sidebar.jsx
@@ -1,14 +1,7 @@
 import React, { useState, useEffect, useRef } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 
-import {
-  FolderArchive,
-  LayoutDashboard,
-  Newspaper,
-  Receipt,
-  Tags,
-  Users2,
-} from "lucide-react";
+import { FolderArchive, LayoutDashboard, Newspaper, Receipt, Tags, Users2 } from "lucide-react";
 
 function Sidebar({ sidebarOpen, setSidebarOpen }) {
   const location = useLocation();
@@ -27,12 +20,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
   useEffect(() => {
     const clickHandler = ({ target }) => {
       if (!sidebar.current || !trigger.current) return;
-      if (
-        !sidebarOpen ||
-        sidebar.current.contains(target) ||
-        trigger.current.contains(target)
-      )
-        return;
+      if (!sidebarOpen || sidebar.current.contains(target) || trigger.current.contains(target)) return;
       setSidebarOpen(false);
     };
     document.addEventListener("click", clickHandler);
@@ -72,9 +60,9 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
       <div
         id="sidebar"
         ref={sidebar}
-        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out ${
+        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-20 lg:sidebar-expanded:!w-64 shrink-0 bg-slate-800 p-4 transition-all duration-200 ease-in-out ${
           sidebarOpen ? "translate-x-0" : "-translate-x-64"
-        }`}
+        } 2xl:w-auto`}
       >
         {/* Sidebar header */}
         <div className="flex justify-between mb-10 pr-3 sm:px-2">
@@ -87,11 +75,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
             aria-expanded={sidebarOpen}
           >
             <span className="sr-only">Close sidebar</span>
-            <svg
-              className="w-6 h-6 fill-current"
-              viewBox="0 0 24 24"
-              xmlns="http://www.w3.org/2000/svg"
-            >
+            <svg className="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
               <path d="M10.7 18.7l1.4-1.4L7.8 13H20v-2H7.8l4.3-4.3-1.4-1.4L4 12z" />
             </svg>
           </button>
@@ -99,23 +83,11 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
           <NavLink end to="/dashboard" className="block">
             <svg width="32" height="32" viewBox="0 0 32 32">
               <defs>
-                <linearGradient
-                  x1="28.538%"
-                  y1="20.229%"
-                  x2="100%"
-                  y2="108.156%"
-                  id="logo-a"
-                >
+                <linearGradient x1="28.538%" y1="20.229%" x2="100%" y2="108.156%" id="logo-a">
                   <stop stopColor="#e50000" stopOpacity="0" offset="0%" />
                   <stop stopColor="#e50000" offset="100%" />
                 </linearGradient>
-                <linearGradient
-                  x1="88.638%"
-                  y1="29.267%"
-                  x2="22.42%"
-                  y2="100%"
-                  id="logo-b"
-                >
+                <linearGradient x1="88.638%" y1="29.267%" x2="22.42%" y2="100%" id="logo-b">
                   <stop stopColor="#" stopOpacity="0" offset="0%" />
                   <stop stopColor="#" offset="100%" />
                 </linearGradient>
@@ -148,9 +120,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
               >
                 •••
               </span>
-              <span className="lg:hidden lg:sidebar-expanded:block 2xl:block">
-                Paginas
-              </span>
+              <span className="lg:hidden lg:sidebar-expanded:block 2xl:block">Paginas</span>
             </h3>
             <ul className="mt-3">
               {/* Dashboard */}
@@ -163,9 +133,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   end
                   to="/dashboard"
                   className={`lg:hidden lg:sidebar-expanded:block text-slate-200 truncate transition duration-150 ${
-                    pathname.includes("messages")
-                      ? "hover:text-slate-200"
-                      : "hover:text-white"
+                    pathname.includes("messages") ? "hover:text-slate-200" : "hover:text-white"
                   }`}
                 >
                   <div className="flex items-center justify-between">
@@ -189,9 +157,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   end
                   to="/news"
                   className={`lg:hidden lg:sidebar-expanded:block text-slate-200 truncate transition duration-150 ${
-                    pathname.includes("messages")
-                      ? "hover:text-slate-200"
-                      : "hover:text-white"
+                    pathname.includes("messages") ? "hover:text-slate-200" : "hover:text-white"
                   }`}
                 >
                   <div className="flex items-center justify-between">
@@ -215,9 +181,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   end
                   to="/categories"
                   className={`lg:hidden lg:sidebar-expanded:block text-slate-200 truncate transition duration-150 ${
-                    pathname.includes("messages")
-                      ? "hover:text-slate-200"
-                      : "hover:text-white"
+                    pathname.includes("messages") ? "hover:text-slate-200" : "hover:text-white"
                   }`}
                 >
                   <div className="flex items-center justify-between">
@@ -241,9 +205,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   end
                   to="/tags"
                   className={`lg:hidden lg:sidebar-expanded:block text-slate-200 truncate transition duration-150 ${
-                    pathname.includes("messages")
-                      ? "hover:text-slate-200"
-                      : "hover:text-white"
+                    pathname.includes("messages") ? "hover:text-slate-200" : "hover:text-white"
                   }`}
                 >
                   <div className="flex items-center justify-between">
@@ -266,9 +228,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   end
                   to="/adds"
                   className={`lg:hidden lg:sidebar-expanded:block text-slate-200 truncate transition duration-150 ${
-                    pathname.includes("messages")
-                      ? "hover:text-slate-200"
-                      : "hover:text-white"
+                    pathname.includes("messages") ? "hover:text-slate-200" : "hover:text-white"
                   }`}
                 >
                   <div className="flex items-center justify-between">
@@ -291,9 +251,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   end
                   to="/team"
                   className={`lg:hidden lg:sidebar-expanded:block text-slate-200 truncate transition duration-150 ${
-                    pathname.includes("messages")
-                      ? "hover:text-slate-200"
-                      : "hover:text-white"
+                    pathname.includes("messages") ? "hover:text-slate-200" : "hover:text-white"
                   }`}
                 >
                   <div className="flex items-center justify-between">
@@ -311,14 +269,11 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
         </div>
 
         {/* Expand / collapse button */}
-        <div className="pt-3 hidden lg:inline-flex 2xl:hidden justify-end mt-auto">
+        <div className="pt-3 hidden lg:inline-flex 3xl:hidden justify-end mt-auto">
           <div className="px-3 py-2">
             <button onClick={() => setSidebarExpanded(!sidebarExpanded)}>
               <span className="sr-only">Expand / collapse sidebar</span>
-              <svg
-                className="w-6 h-6 fill-current sidebar-expanded:rotate-180"
-                viewBox="0 0 24 24"
-              >
+              <svg className="w-6 h-6 fill-current sidebar-expanded:rotate-180" viewBox="0 0 24 24">
                 <path
                   className="text-slate-400"
                   d="M19.586 11l-5-5L16 4.586 23.414 12 16 19.414 14.586 18l5-5H7v-2z"
